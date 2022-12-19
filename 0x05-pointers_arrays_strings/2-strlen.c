2396681d1a3f7ded568d7522cebd8a7fdf779a7d
Alex23603 <Alex24603@gmail.com>
2022-12-19 15:07:49 -0800

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strien -length of a string
+ * @s: this is the input string
+ *
+ * Return: length of the string
+ */
+int _strlem(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0' ; index++)
+
+	return (index);
+}
